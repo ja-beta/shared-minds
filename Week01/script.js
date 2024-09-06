b13c8eb0ef9b277536112546cf2d3476c43c4bde
Jasmine Nackash <jasmine.nackash@gmail.com>
2024-09-06 16:32:38 -0400

    added words functionality instead of letters

--- Week01/script.js
+++ Week01/script.js
@@ -10,10 +10,12 @@ let recentInputLength = 0;
 
 inputBox.addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
-        spreadLettersRandomly();
+        spreadWordsRandomly();
         console.log(letterPositions.size);
     }
 });
+
+// LETTERS
 
 function spreadLettersRandomly() {
     const inputValue = inputBox.value.split('');
@@ -60,6 +62,67 @@ function spreadLettersRandomly() {
 }
 
 function removeLetters(count) {
+    for (let position of usedPositions) {
+        if (count === 0) break;
+        usedPositions.delete(position);
+        letterPositions.delete(position);
+        count--;
+        if (count === 0) break;
+    }
+}
+
+
+// WORDS
+
+function spreadWordsRandomly() {
+    const inputValue = inputBox.value.split(' '); 
+    const numRows = 6; 
+    const numCols = 6; 
+    const numCells = numRows * numCols;
+    cellWidth = canvas.width / numCols;
+    cellHeight = canvas.height / numRows;
+    const minFontSize = 12;
+    const maxFontSize = 32;
+
+    if (inputValue.length + usedPositions.size > numCells) {
+        const wordsToRemove = inputValue.length + usedPositions.size - numCells;
+        removeWords(wordsToRemove);
+    }
+
+    inputValue.forEach(word => {
+        let position, x, y, row, col;
+        do {
+            row = Math.floor(Math.random() * numRows);
+            col = Math.floor(Math.random() * numCols);
+            position = `${row}-${col}`;
+        } while (usedPositions.has(position));
+
+        usedPositions.add(position);
+        letterPositions.set(position, word);
+
+        const fontSize = Math.floor(Math.random() * (maxFontSize - minFontSize + 1)) + minFontSize;
+        ctx.font = `${fontSize}px Helvetica`;
+
+        const clearX = col * cellWidth;
+        const clearY = row * cellHeight;
+
+        ctx.clearRect(clearX, clearY, cellWidth, cellHeight);
+
+        x = col * cellWidth + (cellWidth / 2);
+        y = row * cellHeight + (cellHeight / 2) + (fontSize / 2);
+
+        
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+
+        ctx.fillText(word, x, y);
+    });
+
+    recentInputLength = inputValue.length;
+    inputBox.value = '';
+}
+
+function removeWords(count) {
     for (let position of usedPositions) {
         if (count === 0) break;
         usedPositions.delete(position);
