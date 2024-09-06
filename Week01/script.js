3bf21357c39b3b3491838df6b81aebe50f4e5c09
Jasmine Nackash <jasmine.nackash@gmail.com>
2024-09-06 16:05:07 -0400

    added week01

---
+++ Week01/script.js
@@ -0,0 +1,70 @@
+const inputBox = document.getElementById('inputBox');
+const canvas = document.getElementById('myCanvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const ctx = canvas.getContext('2d'); 
+let usedPositions = new Set(); 
+let letterPositions = new Map(); 
+let cellWidth, cellHeight; 
+let recentInputLength = 0;
+
+inputBox.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        spreadLettersRandomly();
+        console.log(letterPositions.size);
+    }
+});
+
+function spreadLettersRandomly() {
+    const inputValue = inputBox.value.split('');
+    const numRows = 12;
+    const numCols = 12;
+    const numCells = numRows * numCols;
+    cellWidth = canvas.width / numCols;
+    cellHeight = canvas.height / numRows;
+    const minFontSize = 12; 
+    const maxFontSize = 32; 
+
+    if (inputValue.length + usedPositions.size > numCells) {
+        const lettersToRemove = inputValue.length + usedPositions.size - numCells;
+        removeLetters(lettersToRemove);
+    }
+
+    inputValue.forEach(letter => {
+        let position, x, y, row, col;
+        do {
+            row = Math.floor(Math.random() * numRows);
+            col = Math.floor(Math.random() * numCols);
+            position = `${row}-${col}`;
+        } while (usedPositions.has(position));
+
+        usedPositions.add(position);
+        letterPositions.set(position, letter);
+
+        const fontSize = Math.floor(Math.random() * (maxFontSize - minFontSize + 1)) + minFontSize;
+        ctx.font = `${fontSize}px Helvetica`;
+
+        const clearX = col * cellWidth;
+        const clearY = row * cellHeight;
+
+        ctx.clearRect(clearX, clearY, cellWidth, cellHeight);
+
+        x = col * cellWidth + (cellWidth / 2);
+        y = row * cellHeight + (cellHeight / 2) + (fontSize / 2); 
+
+        ctx.fillText(letter, x, y);
+    });
+
+    recentInputLength = inputValue.length;
+    inputBox.value = '';
+}
+
+function removeLetters(count) {
+    for (let position of usedPositions) {
+        if (count === 0) break;
+        usedPositions.delete(position);
+        letterPositions.delete(position);
+        count--;
+        if (count === 0) break;
+    }
+}
